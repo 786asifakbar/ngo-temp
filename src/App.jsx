eb55a887db44f71e7544e6fb087a40277f458fde
Asif Akbar <786asifakbar53@gmail.com>
2026-03-10 20:03:07 +0500

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import Campaigns from "./components/Campaigns"
+import Campaigns from "./pages/Campaigns"
 import Donate from "./components/Donate"
-import Volunteer from "./components/Volunteer"
+import Volunteer from "./pages/Volunteer"
 import About from "./components/About"
 import Team from "./components/Team"
 import Stats from "./components/Stats"
 import Testimonials from "./components/Testimonials"
-import Contact from "./components/Contact"
+import Contact from "./pages/Contact"
 import Footer from "./components/Footer"
 import Donation from "./components/Donation";
 import Impact from "./components/Impact";
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      
       <Campaigns />
       <Donate />
       <Volunteer />
@@ -34,6 +34,33 @@ function App() {
       <Footer />
     </>
   )
+}
+
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Campaigns from "./pages/Campaigns";
+import Contact from "./pages/Contact";
+import Volunteer from "./pages/Volunteer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/campaigns" element={<Campaigns />} />
+        <Route path="/volunteer" element={<Volunteer />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero.jsx";
+
+const Home = () => {
+
+    return (
+    <div className="pt-24">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
